9d5cb0620d25a48a69cb80243930b396742e939f
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-09-29 11:49:26 -0500

    6. Using loop function (for, while)

--- myScript.js
+++ myScript.js
@@ -1,8 +1,14 @@
+/*let lotto = [];
+lotto.push(parseInt(Math.random() * 45 + 1));
+lotto.push(parseInt(Math.random() * 45 + 1));
+lotto.push(parseInt(Math.random() * 45 + 1));
+lotto.push(parseInt(Math.random() * 45 + 1));
+lotto.push(parseInt(Math.random() * 45 + 1));
+lotto.push(parseInt(Math.random() * 45 + 1));
+document.write(lotto)*/
+
 let lotto = [];
-lotto.push(parseInt(Math.random() * 45 + 1));
-lotto.push(parseInt(Math.random() * 45 + 1));
-lotto.push(parseInt(Math.random() * 45 + 1));
-lotto.push(parseInt(Math.random() * 45 + 1));
-lotto.push(parseInt(Math.random() * 45 + 1));
-lotto.push(parseInt(Math.random() * 45 + 1));
+for (let i = 0; i < 6; i++){
+    lotto.push(parseInt(Math.random() * 45 + 1));
+}
 document.write(lotto)
