756130090918e62c979dbad08b00c90ef357c96c
hyangyoungs@gmail.com <hyangyoungs@gmail.com>
2022-09-30 00:07:04 -0500

    6. Using loop function - while, .length

--- myScript.js
+++ myScript.js
@@ -7,11 +7,23 @@ lotto.push(parseInt(Math.random() * 45 + 1));
 lotto.push(parseInt(Math.random() * 45 + 1));
 document.write(lotto)*/
 
+// let lotto = [];
+// for (let i = 0; i < 6; i++){
+//         let num = parseInt(Math.random() * 45 + 1);
+//         if (lotto.indexOf(num) === -1){
+//             lotto.push(num);
+//         }
+//     }
+// document.write(lotto)
+
+// We don't use for. Why? if there is same number generated, it will show 5 numbers not 6.
+
 let lotto = [];
-for (let i = 0; i < 6; i++){
-        let num = parseInt(Math.random() * 45 + 1);
+while(lotto.length < 6 ){
+    let num = parseInt(Math.random() * 45 + 1);
         if (lotto.indexOf(num) === -1){
             lotto.push(num);
         }
     }
+
 document.write(lotto)
